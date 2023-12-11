b8297c35bdb61ee45706f6536b6207a4e82e6360
girvel <widauka@ya.ru>
2023-12-11 18:08:08 +0500

    Libraries: tiny-ecs, luafun, log

--- main.lua
+++ main.lua
@@ -1,3 +1,37 @@
+Log = require "lib.log"
+Fun = require "lib.fun"
+love = love or {}
+
+
+local ecs = require "lib.ecs"
+
+
+local world, player
+
+love.load = function()
+    Log.info("Loading the game")
+
+    world = ecs.world()
+
+    world:add(ecs.processingSystem {
+        filter = ecs.requireAll("counter"),
+
+        process = function(self, entity)
+            entity.counter = entity.counter + 1
+        end
+    })
+
+    player = world:add {
+        counter = 0,
+    }
+
+    Log.info("Finished loading")
+end
+
+love.update = function()
+    world:update()
+end
+
 love.draw = function()
-    love.graphics.print("Hello, world!", 400, 300)
+    love.graphics.print("Counter is " .. player.counter, 400, 300)
 end
